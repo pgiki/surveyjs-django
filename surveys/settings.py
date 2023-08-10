084a20a959b273ae4f8edadbdbdbbb93df6912be
Paschal Giki <paschalgiki@gmail.com>
2023-08-11 00:47:14 +0300

    Add trusted domains

--- surveys/settings.py
+++ surveys/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS=['*']
+CSRF_TRUSTED_ORIGINS=['http://*', 'https://*']
 # Application definition
 INSTALLED_APPS = [
    "admin_interface",
